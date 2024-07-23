130a97f255b771587d6752224510672c1377cd37
tg0xff <tg0xff@proton.me>
2024-07-22 21:57:42 -0600

    Fix OBOE in fibs()

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,9 @@
 function fibs(n) {
   const sequence = [0, 1];
-  for (let i = 2; i < n; i++) {
-    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  if (n <= 1) return sequence.slice(0, 1);
+  if (n === 2) return sequence;
+  for (let i = 3; i <= n; i++) {
+    sequence[i - 1] = sequence[i - 2] + sequence[i - 3];
   }
   return sequence;
 }
