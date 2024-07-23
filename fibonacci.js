10c92168c7aa3346b5da0e1d3a330a22d381514e
tg0xff <tg0xff@proton.me>
2024-07-22 21:30:19 -0600

    Define fibs()

---
+++ fibonacci.js
@@ -0,0 +1,10 @@
+function fibs(n) {
+  const sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    sequence[i] = sequence[i - 1] + sequence[i - 2];
+  }
+  return sequence;
+}
+
+console.log("fibs(8):");
+console.log(fibs(8));
