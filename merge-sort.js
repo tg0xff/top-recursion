71485207ab69b05b5ad8deb1c135b2106f08ca1a
tg0xff <tg0xff@proton.me>
2024-07-22 22:58:27 -0600

    Define first iteration of mergeSort()
    It doesn't work. It exceeds the call stack limit.

---
+++ merge-sort.js
@@ -0,0 +1,30 @@
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+
+  const merged = [];
+  while (left && right) {
+    const leftNum = left[left.length - 1];
+    const rightNum = right[right.length - 1];
+    if (leftNum > rightNum) {
+      merged.push(left.pop());
+    } else {
+      merged.push(right.pop());
+    }
+  }
+
+  if (left) {
+    merged.concat(left);
+  } else if (right) {
+    merged.concat(right);
+  }
+
+  return merged;
+}
+
+console.log("mergeSort([3, 2, 1, 13, 8, 5, 0, 1]):")
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log("mergeSort([105, 79, 100, 110]):")
+console.log(mergeSort([105, 79, 100, 110]));
